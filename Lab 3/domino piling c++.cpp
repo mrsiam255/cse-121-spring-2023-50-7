f39e6b87412aa992635bd0762582c3e6cda67626
Ferdoes Siam <122333286+mrsiam255@users.noreply.github.com>
2023-01-23 21:38:27 +0600

    Add files via upload

---
+++ Lab 3/domino piling c++.cpp
@@ -0,0 +1,15 @@
+// code force code name: A. Domino piling
+// code number:50A
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m,n;
+    cin>> m >> n;
+
+
+    int a=(m*n) / 2;
+    cout<<a;
+    return 0;
+}

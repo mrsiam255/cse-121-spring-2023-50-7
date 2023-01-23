7e19713915decf81961d5e7b2316e5993a0bfd51
Ferdoes Siam <122333286+mrsiam255@users.noreply.github.com>
2023-01-23 21:37:50 +0600

    Add files via upload

---
+++ Lab 1/1.255_50_7.cpp
@@ -0,0 +1,16 @@
+//Codeforce problem no: 4A
+// Problem name: watermelon
+
+
+#include<stdio.h>
+int main()
+{
+    int w;
+    scanf("%d",&w);
+
+    if ( w %2 == 0 && w > 2 )
+        printf("YES");
+    else
+        printf("NO");
+    return 0;
+}

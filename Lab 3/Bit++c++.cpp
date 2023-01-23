d8baa3b6108bd2c254f0facb852535c4aefb008c
Ferdoes Siam <122333286+mrsiam255@users.noreply.github.com>
2023-01-23 21:52:35 +0600

    Add files via upload

---
+++ Lab 3/Bit++c++.cpp
@@ -0,0 +1,30 @@
+// code force code name: A. Bit++
+// code number:282A
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n, x(0);
+    cin >> n;
+
+    string s;
+    while (n--)
+    {
+        cin >> s;
+        if (s[1] == '+')
+        {
+            ++x;
+        }
+        else
+        {
+            --x;
+        }
+    }
+
+    cout << x << endl;
+    return 0;
+}
